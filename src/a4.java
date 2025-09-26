d788c67f0f3ac7ed77a69133cebbfbefd8b8f094
Likeamoon <Sukuba10@gmail.com>
2025-09-26 12:09:00 +0300

    1123s

--- src/a4.java
+++ src/a4.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 public class a4 {
     public static void main(String[] args) {
 
-        String s = "ÐŸÐµÑ€ÐµÐ²Ñ‹Ð±Ð¾Ñ€Ñ‹ Ð²Ñ‹Ð±Ñ€Ð°Ð½Ð½Ð¾Ð³Ð¾ Ð¿Ñ€ÐµÐ·Ð¸Ð´ÐµÐ½Ñ‚Ð°";
+        String s = "2123";
 
         int count = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'Ðµ') {
+            if (s.charAt(i) == '1') {
                 count++;
             }
         }
 
-        System.out.println("ÐšÐ¾Ð»Ð¸Ñ‡ÐµÑÑ‚Ð²Ð¾ Ð±ÑƒÐºÐ² 'Ðµ': " + count);
+        System.out.println("33333333333" + count);
     }
 }
